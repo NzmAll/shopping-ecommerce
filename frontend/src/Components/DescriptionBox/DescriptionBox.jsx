357494b17f65bfb7c17f805792358a8a61c16de1
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2024-02-13 20:31:20 +0400

    DescriptionBox section added

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore,
+          repellat! Officia eveniet veritatis ipsum quam molestias deleniti
+          ratione neque quaerat magni harum tempore nobis labore numquam,
+          consequuntur inventore tenetur cum. Non exercitationem unde voluptate
+          illo aperiam facilis nihil tenetur maiores, sint, architecto dolore
+          assumenda? Neque placeat, tempore doloribus labore velit quidem culpa
+          tempora asperiores minus veritatis exercitationem. Illum minus,
+          laboriosam consequatur itaque unde, eius a illo ipsum, sapiente
+          laudantium cumque.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dolorem
+          consectetur vel deleniti, labore rerum accusantium aspernatur
+          consequatur amet aliquam accusamus molestiae ullam non sapiente aut,
+          iste laudantium dolorum eos.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;

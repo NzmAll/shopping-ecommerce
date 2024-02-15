7d8756b4a7a444cdab9cd6329b6c486015b41d44
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2024-02-15 10:14:42 +0400

    Create API For getting all products

--- backend/index.js
+++ backend/index.js
@@ -120,6 +120,14 @@ app.post("/removeproduct", async (req, res) => {
   });
 });
 
+// Create API For getting all products
+
+app.get("./allproducts", async (req, res) => {
+  let products = await Product.find({});
+  console.log("All Products Fetched");
+  res.send(products);
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server running on Port " + port);

ed0d1057ab5d6ea00496747fbfc201deeb9052e7
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2024-02-15 12:43:36 +0400

    Admin Panel Sidebar section added

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import "./Admin.css"
+import React from "react";
+import "./Admin.css";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 
 const Admin = () => {
   return (
-    <div className='admin'>
-        
+    <div className="admin">
+      <Sidebar />
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;

b8bfcece8b12006c181dbf0b5e32982b9c510c26
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2024-02-16 17:52:20 +0400

    Creating endpoint to remove Product from Cartdata

--- backend/index.js
+++ backend/index.js
@@ -244,6 +244,7 @@ const fetchUser = async (req, res, next) => {
 //creating endpoint for adding products in cartdata
 
 app.post("/addtocart", fetchUser, async (req, res) => {
+  console.log("Added", req.body.itemId);
   let userData = await Users.findOne({ _id: req.user.id });
   userData.cartData[req.body.itemId] += 1;
   await Users.findOneAndUpdate(
@@ -251,6 +252,20 @@ app.post("/addtocart", fetchUser, async (req, res) => {
     { cartData: userData.cartData }
   );
   res.send("Added");
+});
+
+//creating endpoint to remove product from cartdata
+
+app.post("/removefromcart", fetchUser, async (req, res) => {
+  console.log("removed", req.body.itemId);
+  let userData = await Users.findOne({ _id: req.user.id });
+  if (userData.cartData[req.body.itemId] > 0)
+    userData.cartData[req.body.itemId] -= 1;
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Removed");
 });
 
 app.listen(port, (error) => {
